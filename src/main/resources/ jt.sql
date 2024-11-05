341cfe441b7890162447749a8de846a708a7de77
Vincent C <vinch.cm@gmail.com>
2024-11-05 14:42:18 +0100

    jt.sql file created to create db and tables

---
+++ src/main/resources/ jt.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE JT;
+
+CREATE TABLE CUSTOMER (
+    ID integer primary key not null AUTO_INCREMENT,
+    EMAIL varchar(50)
+);
+
+CREATE TABLE FEEDBACK (
+    ID integer primary key not null AUTO_INCREMENT,
+    CONTENT varchar(50),
+    TYPE varchar(10),
+    CUSTOMER_ID integer,
+    CONSTRAINT customer_fk FOREIGN KEY (CUSTOMER_ID) REFERENCES CUSTOMER(ID)
+);
+
+SHOW TABLES;
